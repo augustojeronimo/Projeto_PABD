cb2d7df642837001f59ae0d2fb60402fb5c32e22
Augusto Jerônimo <dev.augustoj@gmail.com>
2023-12-16 23:56:08 -0300

    criação da classe de acesso ao banco

---
+++ vendaDeDindin/src/br/edu/ifrn/banco/AcessoBD.java
@@ -0,0 +1,16 @@
+package br.edu.ifrn.banco;
+
+
+public class AcessoBD {
+    private class ConexaoDindin {
+        
+    }
+    
+    private class ConexaoVenda {
+        
+    }
+    
+    private class ConexaoDindinVendido {
+        
+    }
+}
